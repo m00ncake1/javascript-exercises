2c58d7d106009873795eaf99232b07315b724b5e
Imaan <imaansayed02@gmail.com>
2023-12-28 00:24:49 +0200

    Learnt: Filter, reduce, spread [...x] (Completed for ArraysAndLoops section)

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, reps) {
+    let accum = '';
+    if (reps<0){
+        return "ERROR";
+    }
+    for (let i=0; i<reps; i++){
+        accum+=string;
+    }
 
+    return accum;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    if (!word){
+        return "";
+    }
+    word= String(word)
+    let reversed = [...word].reduce( (build, prevLetter) =>prevLetter + build)
+    return reversed
 };
 
 // Do not edit below this line
